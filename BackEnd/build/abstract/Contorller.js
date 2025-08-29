dd8e741726f104a229c9043bedf3540c048bccce
Chun-Lin-Huang <elaine930729@gmail.com>
2025-08-29 21:33:23 +0800

    20250829

--- BackEnd/build/abstract/Contorller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Contorller = void 0;
-class Contorller {
-    constructor() {
-    }
-}
-exports.Contorller = Contorller;
